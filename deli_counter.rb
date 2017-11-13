cef9f53a4f5b190177ff8c1a9cc430fee6442923
Tyler Pulse <tyler3321@gmail.com>
2017-11-13 17:48:12 +0000

    automatically backed up by learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -16,7 +16,7 @@ def take_a_number(katz_deli, name)
   if katz_deli == [""]
     array = []
     katz_deli.each_with_index {|index, value| array << "Welcome, #{index}. You are number#{value+1} in line"}
-    puts "Welcome #{array}, you are number #{array} in line."
+    puts "Welcome #{array}, you are number #{array} in line." << "#{name}"
 
   end
 end
