c28d76b17cf3c5b032b69ea98fb9262f2200f753
Tyler Pulse <tyler3321@gmail.com>
2017-11-13 17:14:36 +0000

    automatically backed up by learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -15,7 +15,7 @@ def take_a_number(line, name)
   case line
 
   when line = []
-    line.each_with_index{|index, value| "#{value+1}#{index}"
+    line.each_with_index{|index, value| "#{value+1}#{index}"}
     puts "Welcome, #{index}. You are number #{value} in line."
   end
 end
