8072bc55cf782ad78d87b90cdda6e20b06fdc3a6
Tyler Pulse <tyler3321@gmail.com>
2017-11-13 17:08:09 +0000

    automatically backed up by learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -1,1 +1,12 @@
-# Write your code here.
+def line(katz_deli)
+
+  if katz_deli == []
+    puts "The line is currently empty."
+  else
+      katz_deli == [""]
+      array = []
+      katz_deli.each_with_index {|index, value| array << " #{value+1}. #{index}"}
+      puts "The line is currently:#{array.join}"
+
+    end
+end
